149469fa58cb376170c4303a7800af24408786ca
Bedford Michael <kestereliemenye@gmail.com>
2024-03-04 15:14:18 -0800

    creating total expense

--- functions.js
+++ functions.js
@@ -1,9 +1,26 @@
 function getExpenseDataFromUI() {
     const name = document.querySelector('#student-name').value
     const className = document.querySelector('#class').value
-    const amount = document.querySelector('#cash').value
+    const cashArray = [];// initialized an empty array to me used
+    let cashValue = parseInt(document.querySelector('#cash').value);
+    function addValue(){
+        cashArray.push(cashValue);// assigned the values to the array
+        console.log(cashArray)
+        calculatetotal();
+        return cashValue;
+        };
+        addValue();// i  ran the funtion
+        function calculatetotal(){ // function to loop to get the total
+            let total = 0;
+            for (let i = 0; i < cashArray.length; i++ ) {
+                total += cashArray[i];
+                console.log(total)
+            } 
+        };
+
+    
     const reason = document.querySelector('#expense').value
-    return { name, className, amount, reason } // <--- this is a returned object
+    return { name, className, cashValue, reason} // <--- this is a returned object
 }
 //worked on the clear button
 const clearBtn = document.querySelector('.clear-button')
@@ -14,26 +31,24 @@ const totalExp = document.querySelector('.total-expense');
 // this function will add the expense to the table
 function addExpenseToTable() {
     const exp = getExpenseDataFromUI() // you see this i am calling the function getExpenseDataFromUI I created above here
-    if ( exp.name === '' || exp.className === '' || exp.amount === '' || exp.reason === '') {
+    if ( exp.name === '' || exp.className === '' || exp.cashValue === '' || exp.reason === '') {
         alert('Please fill in all fields')
         return // notice the empty return statement here. Why is this necessary here? Can you tell me? If you dont know try to remove the return statement and see what happens when you click the submit button. What changed?
     }
     
-    let cashAmount = parseInt(exp.amount);//i made the cash input numbers only
 
     const newRow = document.createElement('tr')
-    if (isNaN(cashAmount)){
-        alert('Cash is not a number');
-        return
-    }
+    // if (isNaN()){
+    //     alert('Cash is not a number');
+    //     return
+    // }
     newRow.innerHTML = `
     <td>${exp.name}</td>
     <td>${exp.className}</td>
-    <td>${cashAmount}</td>
+    <td>${exp.cashValue}</td>
       <td>${exp.reason}</td>
       `
-      
-    
+
     document.querySelector('tbody').appendChild(newRow)
     document.querySelector('#student-name').value = ''
     document.querySelector('#class').value = ''
@@ -44,10 +59,7 @@ function addExpenseToTable() {
     function clearWhole (e){
         newRow.remove();
     }
-
-   
   
-
 }
 const submitExpense = document.querySelector('.go')
 submitExpense.addEventListener('click', addExpenseToTable)
